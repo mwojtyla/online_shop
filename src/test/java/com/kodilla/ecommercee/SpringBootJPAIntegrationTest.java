759231478cf8690dadf5ae2d4fff37b329625a49
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-04-03 15:00:15 +0200

    JDP230402-4 Added basic endpoints implementation for OrderController and classes: OrderDto and OrderItemDto

--- src/test/java/com/kodilla/ecommercee/SpringBootJPAIntegrationTest.java
+++ src/test/java/com/kodilla/ecommercee/SpringBootJPAIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.GenericEntity;
+import com.kodilla.ecommercee.repository.GenericEntityRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
