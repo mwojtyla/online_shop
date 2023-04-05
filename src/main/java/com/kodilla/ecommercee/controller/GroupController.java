4e01c22c5e282b79b6971c68f0b929fbd66e54b6
Andrzej Jaromin <ajaromin@silenteight.com>
2023-04-05 18:31:29 +0200

    Test commit 2

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,4 +1,9 @@
 package com.kodilla.ecommercee.controller;
 
 public class GroupController {
+    private int field1;
+
+    public GroupController(int field1) {
+        this.field1 = field1;
+    }
 }
