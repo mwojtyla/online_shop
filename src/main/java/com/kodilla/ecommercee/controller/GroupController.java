5aea54f308df4e373bf97717a8d6d793c376ef80
Andrzej Jaromin <ajaromin@silenteight.com>
2023-04-05 18:40:14 +0200

    Revert changes

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,13 +1,4 @@
 package com.kodilla.ecommercee.controller;
 
 public class GroupController {
-    private String test1;
-    private String test2;
-    private int field1;
-
-    public GroupController(String test1, String test2, int field1) {
-        this.test1 = test1;
-        this.test2 = test2;
-        this.field1 = field1;
-    }
 }
