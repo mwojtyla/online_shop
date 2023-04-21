d1b41b4c1f481363a6ea7d200a48bfcff9313ea5
Kamil Bonar <kamilbr@op.pl>
2023-04-21 23:04:58 +0200

    New changes in tests

--- src/test/java/com/kodilla/ecommercee/domain/GroupEntityTests.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupEntityTests.java
@@ -2,13 +2,14 @@ package com.kodilla.ecommercee.domain;
 
 import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -38,31 +39,34 @@ public class GroupEntityTests {
         Long groupId = group.getGroupId();
 
         //Then
-        Assertions.assertNotEquals(0, groupId);
+        assertNotEquals(0, groupId);
+        assertTrue(groupRepository.existsById(groupId));
     }
 
     @Test
     public void testDeleteGroup() {
+
         //Given
         Group group = new Group();
-        group.setName("Ubrania");
         Product product = new Product();
-        group.getProducts().add(product);
-        product.setGroup(group);
+        List<Product> productList = new ArrayList<>();
+        productList.add(product);
+        productRepository.save(product);
+        group.setProducts(productList);
+        group.setName("Ubrania");
 
         //When
         groupRepository.save(group);
         Long groupId = group.getGroupId();
-        Long productId = product.getProductId();
         groupRepository.deleteById(groupId);
 
         //Then
-        Assertions.assertFalse(groupRepository.existsById(groupId));
-        Assertions.assertEquals(productId, product.getProductId());
+        assertEquals(Optional.empty(), groupRepository.findById(groupId));
     }
 
     @Test
     public void testUpdateGroup() {
+
         //Given
         Group group = new Group();
 
@@ -75,12 +79,63 @@ public class GroupEntityTests {
 
         //When
         groupRepository.save(group);
-        long groupId = group.getGroupId();
 
         //Then
-        Assertions.assertEquals("Obuwie", group.getName());
+        assertEquals("Obuwie", group.getName());
+    }
 
-        //CleanUp
-        groupRepository.deleteById(groupId);
+    @Test
+    public void testFindById(){
+
+        //Given
+        Group group = new Group();
+
+        Product product = new Product();
+        List<Product> productList = new ArrayList<>();
+        productList.add(product);
+        productRepository.save(product);
+        group.setProducts(productList);
+        group.setName("Test Group");
+
+        //When
+        groupRepository.save(group);
+
+        //Then
+        Long id = group.getGroupId();
+        Optional<Group> groupOptional = groupRepository.findById(id);
+        assertEquals(id,groupOptional.get().getGroupId());
+    }
+
+    @Test
+    public void testSetNameGroup() {
+
+        //Given
+        Group groupTest = new Group();
+        groupTest.setName("New Name");
+
+        //When
+        groupRepository.save(groupTest);
+        String name = groupTest.getName();
+
+        //Then
+        assertEquals("New Name", name);
+    }
+
+    @Test
+    public void testSetProducts() {
+
+        //Given
+        Group groupTest = new Group();
+        Product product1 = new Product();
+        product1.setName("product 1");
+        product1.setGroup(groupTest);
+        productRepository.save(product1);
+
+        //When
+        groupTest.getProducts().add(product1);
+        Group savedGroupTest1 = groupRepository.save(groupTest);
+
+        //Then
+        assertTrue(savedGroupTest1.getProducts().contains(product1));
     }
 }
